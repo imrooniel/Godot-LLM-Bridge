6efc38c7d0ee3dc22bbfe70edc62452031bb1744
Patryk Kowalik <imrooniel@gmail.com>
2026-08-31 12:12:58 +0200

    Restructure repo: make root a Godot project and remove examples/ folder to eliminate duplications

--- examples/minimal_project/addons/editor_bridge/commands/action_scene_save_all.gd
+++
@@ -1,9 +0,0 @@
-@tool
-extends RefCounted
-
-## action.scene.save_all — save all open scenes.
-
-func execute(_args: Dictionary, ctx) -> Dictionary:
-	var ei = ctx.editor_interface
-	ei.save_all_scenes()
-	return {"ok": true, "count": ei.get_open_scenes().size()}
--- examples/minimal_project/addons/editor_bridge/commands/query_editor.gd
+++
@@ -1,9 +0,0 @@
-@tool
-extends RefCounted
-
-## query.editor — editor process info (pid, project, versions).
-
-func execute(_args: Dictionary, ctx) -> Dictionary:
-	var result: Dictionary = {"ok": true}
-	result.merge(ctx.get_editor())
-	return result
